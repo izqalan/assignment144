e710ceb857d8bd422ab2064d7299516f50362cf0
izqalan <izqalan@gmail.com>
2017-07-24 22:40:20 +0800

    Add files via upload
    Fixed reverse array

--- assignment/Source.cpp
+++ assignment/Source.cpp
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <iostream>
 
-int revArray()	// wip. need help. stack error. var rev corrupted
+
+int revArray()	// fixed
 {
-	int rev[6] = {};
+	int rev[7] = {};
 	int sum = 0;
 	for (int i = 0; i < 7; i++)
 	{
 		printf("Enter a value ");
 		scanf_s("%d", (rev + i));
 	}
-	for (int i = 6; i >= 0; i--)
+	for (int i = 7 - 1; i >= 0; i--)
 	{
 		printf("reversed number %d\t\n", rev[i]);
 	}
@@ -40,7 +41,7 @@ int primeFactors()   //opt = 1
 		// 2nd digit
 		for (int i = 3; i <= sqrt(n); i = i + 2)
 		{
-			// While i divides n, print i and divide n
+			// While i mod n, print i and divide n
 			while (n%i == 0)
 			{
 				printf("%d X ", i);
@@ -53,51 +54,22 @@ int primeFactors()   //opt = 1
 		// 3rd digit
 		if (n > 2)
 		{
-			printf("%d", n);
+			printf("%d = %d\n ", n, oldnum);
 
 		}
-		printf(" = %d\n", oldnum);
 	}
 
 	return 0;
 }
 
-/*
-int reverse()	// opt = 2
-{
-	// need to update; use array.
-	//eqn
-	// a=a*10+(x%10)
-	// x/=10
-	int num, rev;
-	rev = 0;
-	printf("Enter 7 digits\n");
-	scanf_s("%d", &num);
-
-	while (num < 1000000)
-	{
-		printf("not enough digits\nEnter 7 digits\n");
-		scanf_s("%d", &num);
-	}
-
-	for (; num != 0;)
-	{
-		rev = rev * 10 + (num % 10);
-		num = num / 10;
-	}
-
-	printf("reverse number is %d", rev);
-	return 0;
-} */
-
 int sum()	//opt = 3
 {
-	
+	//sum loader
 	int num, total;
 	total = 0;
 	for (int i = 0; i < 7; i++)
 	{
-		//sum loader
+		
 		printf("Enter a positive integers\n");
 		scanf_s("%d", &num);
 		total = num + total;
@@ -109,33 +81,28 @@ int sum()	//opt = 3
 int main()
 {
 	int opt;
-	printf("Choose an operation\n press 1 for Prime factors\n press 2 to reverse the number\n press 3 for summation\n press 0 to exit\n");
+	printf("\nChoose an operation\n press 1 for Prime number\n press 2 to reverse the number\n press 3 for summation\n press 0 to exit\n");
 	scanf_s("%d", &opt);
 
 	while (opt != 0)
 	{
-		if (opt == 1)
+		switch (opt)
 		{
+		case 1:
 			primeFactors();
-			system("pause");
-		}
-		else if (opt == 2)
-		{
+			break;
+		case 2:
 			revArray();
-			system("pause");
-		}
-		else if (opt == 3)
-		{
+			break;
+		case 3:
 			sum();
-			system("pause");
-
+			break;
+		default:
+			printf("Invalid Operation\n");
+			break;
 		}
-		else
-		{
-			printf("Invalid option !!\n");
-			system("pause");
 
-		}
+		
 		printf("Choose an operation\n press 1 for Prime number\n press 2 to reverse the number\n press 3 for summation\n press 0 to exit\n");
 		scanf_s("%d", &opt);
 	}
