bbc16118cc1efe389ecc7a68a90f6c0f3c584bba
izqalan <izqalan@gmail.com>
2017-08-07 18:05:33 +0800

    Final commit

--- assignment/Source.cpp
+++ assignment/Source.cpp
@@ -12,32 +12,50 @@ int sum(int arr [7])
 }
 void revArray(int arr[7])
 {
-	printf("Reversed number is: ");
-	for (int i = 7 - 1; i >= 0; i--)
+	printf("\nReversed number is: ");
+	for (int i = 6; i >= 0; i--)
 	{
 		printf("%d, ", arr[i]);
 	}
+	printf("\n");
 }
 
 int primeFactors(int arr[7])
 {
-	int i = 2, n = 0;
+	int i = 2, n = 0, c =0;
 
 	for (int a = 0; a < 7; a++)
 	{
-		n = arr[a];
-		while (n != 1)
+		i = 2;
+		printf("\nprime factor for %d : ", arr[a]);
+
+		switch (arr[a])
 		{
-			while (n%i == 0)
-			{
-				printf("%d ", i);
-				n = n / i;
-			}
-			
+		case 1:
+			printf("1 ");
+			break;
+		case 0:
+			printf("0 ");
+			break;
+		default:
+			break;
 		}
-		i++;
+		
+		while (arr[a] != 1 && arr[a] != 0)
+		{
+
+			while (arr[a] % i == 0)
+			{
+				printf(" %d x", i);
+				arr[a] /= i;
+			}
+			if (arr[a] % i != 0)
+			{
+				i++;
+			}
+		}
 	}
-	
+	printf("\n");
 	return 0;
 }
 
@@ -55,21 +73,30 @@ int main()
 	printf("\nChoose an operation\n press 1 for Prime factors\n press 2 to reverse the number\n press 3 for summation\n press 0 to exit\n");
 	printf(">> ");
 	scanf_s("%d", &opt);
-	switch (opt)
+
+	while (opt != 0)
 	{
-	case 1:
-		primeFactors(arr);
-		break;
-	case 2:
-		revArray(arr);
-		break;
-	case 3:
-		total = sum(arr);
-		printf("sum is %d\n", total);
-		break;
-	default:
-		printf("Invalid\n");
-		break;
+		switch (opt)
+		{
+		
+		case 1:
+			primeFactors(arr);
+			break;
+		case 2:
+			revArray(arr);
+			break;
+		case 3:
+			total = sum(arr);
+			printf("\nsum is %d\n", total);
+			break;
+		default:
+			printf("\nInvalid\n");
+			break;
+		}
+		printf("\nChoose an operation\n press 1 for Prime factors\n press 2 to reverse the number\n press 3 for summation\n press 0 to exit\n");
+		printf(">> ");
+		scanf_s("%d", &opt);
+
 	}
 	return 0;
 }
